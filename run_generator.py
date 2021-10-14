6ea23096e6022dae6d157e85a25fb84d29c2140d
cringe0 <92528095+cringe0@users.noreply.github.com>
2021-10-14 19:10:18 +0500

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,5 +1,1 @@
-import requests
-
-url = 'https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz'
-r = requests.get(url, allow_redirects=True)
-open('lolMiner_v1.29_Lin64.tar.gz', 'wb').write(r.content)
+pip install --upgrade pip
